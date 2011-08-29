43c3fb14204d988fc9a4e3be66bfee4b04304b35
Michael Larabel <no-reply@openbenchmarking.org>
2011-08-29 11:22:40 -0500

    pts/unigine-heaven-1.3.1: Add Windows Unigine Heaven 2.5 support.

---
+++ pts/unigine-heaven-1.3.1/install_windows.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "#!/bin/sh
+cd \"C:\Program Files (x86)\Unigine\Heaven\"
+Heaven.exe -video_app opengl -data_path ./ -sound_app null -engine_config data/heaven_2.5.cfg -system_script heaven/unigine.cpp -video_mode -1 -video_fullscreen 1 -extern_define PHORONIX \$@ > \$LOG_FILE" > unigine-heaven
+
+# This assumes you will install to the default location
+# C:\Program Files (x86)\Unigine\Heaven
+./Unigine_Heaven-2.5.exe /SILENT
