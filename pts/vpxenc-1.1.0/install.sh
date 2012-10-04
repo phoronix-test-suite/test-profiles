1b961207c3bb93098155182e20fcad9256e7a542
Michael Larabel <no-reply@openbenchmarking.org>
2012-10-03 21:43:04 -0500

    pts/vpxenc-1.1.0: Update against upstream VP8 libvpx 1.1.0 release.

---
+++ pts/vpxenc-1.1.0/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+tar -xjf libvpx-v1.1.0.tar.bz2
+
+cd libvpx-v1.1.0/
+./configure --disable-install-docs
+make -j $NUM_CPU_JOBS
+echo $? > ~/install-exit-status
+cd ~
+
+echo "#!/bin/sh
+cd libvpx-v1.1.0/
+./vpxenc --best --psnr -v -t \$NUM_CPU_CORES -o /dev/null ~/soccer_4cif.y4m 2> \$LOG_FILE 
+echo \$? > ~/test-exit-status" > vpxenc
+chmod +x vpxenc
