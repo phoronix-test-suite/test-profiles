5dfaba4ce3b0da091556e264c91b223719e1075b
Michael Larabel <no-reply@openbenchmarking.org>
2013-06-09 14:48:34 -0500

    pts/xonotic-1.3.0: Update xonotic test profile against upstream Xonotic 0.7.0 game release.

---
+++ pts/xonotic-1.3.0/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+unzip -o xonotic-0.7.0.zip
+mv Xonotic Xonotic_
+
+echo "#!/bin/sh
+cd Xonotic_/
+if [ \$OS_TYPE = \"MacOSX\" ]
+then
+	./Xonotic.app/Contents/MacOS/xonotic-osx-sdl \$@ > \$LOG_FILE 2>&1
+else
+	./xonotic-linux-glx.sh \$@ > \$LOG_FILE 2>&1
+fi" > xonotic
+chmod +x xonotic
