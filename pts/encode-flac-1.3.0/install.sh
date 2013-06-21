471695b57b8e0624d950a6952c48e613e9515134
Michael Larabel <no-reply@openbenchmarking.org>
2013-06-21 13:40:13 -0500

    pts/encode-flac-1.3.0: Update against upstream Xiph.Org FLAC 1.3.0.

---
+++ pts/encode-flac-1.3.0/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir $HOME/flac_
+tar -xJf flac-1.3.0.tar.xz
+
+cd flac-1.3.0/
+./configure --prefix=$HOME/flac_
+make -j $NUM_CPU_JOBS
+echo $? > ~/install-exit-status
+make install
+cd ~
+rm -rf flac-1.3.0/
+rm -rf flac_/share/
+
+echo "#!/bin/sh
+./flac_/bin/flac --best \$TEST_EXTENDS/pts-trondheim.wav -f -o /dev/null 2>&1
+echo \$? > ~/test-exit-status" > encode-flac
+chmod +x encode-flac
