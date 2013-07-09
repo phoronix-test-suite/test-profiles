d896371283317700d05e9a41fc8d4e002f8ba3ad
Michael Larabel <no-reply@openbenchmarking.org>
2013-07-09 10:00:24 -0500

    pts/unvanquished-1.0.1: Fix for the Apple OS X launcher of Unvanquished game.

---
+++ pts/unvanquished-1.0.1/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+unzip -o unvanquished-0.17-universal.zip
+mv unvanquished unvanquished-game
+
+unzip -o unvanquished-17-1.zip
+mkdir unvanquished-game/main/demos/
+mv pts.cfg unvanquished-game/main/
+mv pts1.dm_86 unvanquished-game/main/demos/
+
+echo "#!/bin/sh
+cd unvanquished-game/
+if [ \$OS_TYPE = \"MacOSX\" ]
+then
+	./Unvanquished.app/Contents/Resources/Unvanquished/daemon +set fs_libpath \$HOME/unvanquished-game/ \$@ > \$LOG_FILE 2>&1
+else
+	./daemon \$@ > \$LOG_FILE 2>&1
+fi" > unvanquished
+chmod +x unvanquished
