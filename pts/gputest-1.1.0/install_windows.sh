38523eff525acc2837992105fcebc96f23ecdd54
Michael Larabel <no-reply@openbenchmarking.org>
2013-07-13 10:31:12 -0500

    pts/gputest-1.1.0: Update against upstream GpuTest 0.5.0, also integrate Windows support.

---
+++ pts/gputest-1.1.0/install_windows.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+unzip -o GpuTest_Windows_x64_0.5.0.zip
+
+echo "#!/bin/sh
+rm -f _geeks3d_gputest_log.txt
+GpuTest.exe \$@ 
+cat _geeks3d_gputest_log.txt > \$LOG_FILE" > gputest
+chmod +x gputest
