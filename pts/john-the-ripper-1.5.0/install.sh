fef6433d4fe3bb6e59a6436f48385867cf3ddbb7
Michael Larabel <no-reply@openbenchmarking.org>
2014-01-10 13:09:42 -0600

    pts/john-the-ripper-1.5.0: Update against upstream John The Ripper 1.8.0.

---
+++ pts/john-the-ripper-1.5.0/install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+tar -zxvf john-1.8.0.tar.gz
+cd john-1.8.0/src/
+
+case $OS_TYPE in
+	"MacOSX")
+		make OMPFLAGS=-fopenmp macosx-x86-64
+	;;
+	"Solaris")
+		if [ $OS_ARCH = "x86_64" ]
+		then
+			make OMPFLAGS=-fopenmp solaris-x86-64-gcc
+		else
+			make OMPFLAGS=-fopenmp solaris-x86-sse2-gcc
+		fi
+	;;
+	"BSD")
+		if [ $OS_ARCH = "x86_64" ]
+		then
+			make OMPFLAGS=-fopenmp freebsd-x86-64
+		else
+			make OMPFLAGS=-fopenmp freebsd-x86-sse2
+		fi
+	;;
+	*)
+		if [ $OS_ARCH = "x86_64" ]
+		then
+			make OMPFLAGS=-fopenmp linux-x86-64
+		else
+			make OMPFLAGS=-fopenmp linux-x86-sse2
+		fi
+	;;
+esac
+
+cd ~/
+
+echo "#!/bin/sh
+cd john-1.8.0/run/
+./john --test > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > john-the-ripper
+chmod +x john-the-ripper
