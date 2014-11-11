5367d9cc3392ee46c629b6aeae44a5b51563cf8b
Michael Larabel <no-reply@openbenchmarking.org>
2014-11-11 08:16:39 -0600

    pts/tf2-1.0.2: Path updates to let TF2 run on latest Steam Linux client.

---
+++ pts/tf2-1.0.2/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+unzip -o tf2-demo-1.zip
+mv pts2.dem $DEBUG_REAL_HOME/.steam/steam/SteamApps/common/Team\ Fortress\ 2/tf
+
+echo "#!/bin/sh
+cd \$DEBUG_REAL_HOME/.steam/steam/SteamApps/common/Team\ Fortress\ 2
+
+LD_LIBRARY_PATH=\$DEBUG_REAL_HOME/.steam/steam/SteamApps/common/Team\ Fortress\ 2/bin:\$DEBUG_REAL_HOME/.steam/ubuntu12_32:\$DEBUG_REAL_HOME/.steam/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu:\$DEBUG_REAL_HOME/.steam/ubuntu12_32/steam-runtime/i386/lib:\$DEBUG_REAL_HOME/.steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu:\$DEBUG_REAL_HOME/.steam/ubuntu12_32/steam-runtime/i386/usr/lib:\$DEBUG_REAL_HOME/.steam/ubuntu12_32/steam-runtime/amd64/lib/x86_64-linux-gnu:\$DEBUG_REAL_HOME/.steam/ubuntu12_32/steam-runtime/amd64/lib:\$DEBUG_REAL_HOME/.steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu:\$DEBUG_REAL_HOME/.steam/ubuntu12_32/steam-runtime/amd64/usr/lib:/usr/lib32:\$DEBUG_REAL_HOME/.steam/ubuntu12_32:\$DEBUG_REAL_HOME/.local/share/Steam/ubuntu12_64:\$DEBUG_REAL_HOME/.steam/steam/SteamApps/common/Team\ Fortress\ 2:\$DEBUG_REAL_HOME/.steam/steam/SteamApps/common/Team\ Fortress\ 2/bin ./hl2_linux -game tf +con_logfile \$LOG_FILE +cl_showfps 1 -fullscreen -novid +timedemoquit pts2 \$@" > tf2
+chmod +x tf2
