1aab8b6de6d96f4ac65f0be2c26b2245a5f7801c
Michael Larabel <no-reply@openbenchmarking.org>
2015-01-22 16:36:39 -0600

    pts/unigine-tropics-1.6.0: Add windowed mode

---
+++ pts/unigine-tropics-1.6.0/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+chmod +x Unigine_Tropics-1.3.run
+./Unigine_Tropics-1.3.run
+
+UNIGINE_ARGS=\$@
+if [[ \$ARGS != *\"-video_fullscreen\"* ]]
+then
+	UNIGINE_ARGS=\"\$UNIGINE_ARGS -video_fullscreen 1\"
+fi
+
+echo "#!/bin/sh
+cd tropics/
+export LD_LIBRARY_PATH=bin/:\$LD_LIBRARY_PATH
+./bin/Tropics -video_app opengl -data_path ../ -sound_app null -system_script tropics/unigine.cpp -engine_config ../data/unigine.cfg -video_mode -1 -video_multisample 0 -extern_define PHORONIX \$UNIGINE_ARGS > \$LOG_FILE 2>&1" > unigine-tropics
+chmod +x unigine-tropics
+
