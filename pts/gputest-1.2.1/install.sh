672c81d386b32d96943c9ed96a5cb658e530155b
Michael Larabel <no-reply@openbenchmarking.org>
2015-05-08 12:55:48 -0500

    pts/gputest-1.2.1: Add windowed mode option.

---
+++ pts/gputest-1.2.1/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+unzip -o GpuTest_Linux_x64_0.6.0.zip
+mv GpuTest GpuTest_Bin
+chmod +x GpuTest_Bin
+
+echo "#!/bin/sh
+rm -f _geeks3d_gputest_log.txt
+./GpuTest_Bin \$@ 
+cat _geeks3d_gputest_log.txt > \$LOG_FILE" > gputest
+chmod +x gputest
