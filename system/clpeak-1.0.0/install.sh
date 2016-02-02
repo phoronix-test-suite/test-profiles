175b1f2eb4c52765e4e9bdce07f3a37d2478b3cd
Michael Larabel <no-reply@openbenchmarking.org>
2016-02-02 11:09:30 -0600

    system/clpeak-1.0.0: Initial commit of clpeak test profile for OpenCL peaks

---
+++ system/clpeak-1.0.0/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if which clpeak >/dev/null 2>&1 ;
+then
+	echo 0 > ~/install-exit-status
+else
+	echo "ERROR: clpeak is not found on the system!"
+	echo 2 > ~/install-exit-status
+fi
+
+echo "#!/bin/sh
+clpeak \$@ > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > clpeak
+chmod +x clpeak
+
+
