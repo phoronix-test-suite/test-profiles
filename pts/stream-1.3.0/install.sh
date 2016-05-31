df5d9fdef69d4a8f9b32a37a7cffab778e8ba865
Michael Larabel <no-reply@openbenchmarking.org>
2016-05-31 09:25:02 -0500

    pts/stream-1.3.0: Increase array size, other minor adjustments

---
+++ pts/stream-1.3.0/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+tar -jxf stream-2013-01-17.tar.bz2
+if [ "X$CFLAGS" = "X" ]
+then
+	CFLAGS="-O3 -march=native"
+fi
+cc stream.c -DSTREAM_ARRAY_SIZE=100000000 -DNTIMES=100 $CFLAGS -fopenmp -o stream-bin
+echo \$? > ~/install-exit-status
+
+echo "#!/bin/sh
+export OMP_NUM_THREADS=\$NUM_CPU_CORES
+./stream-bin > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > stream
+chmod +x stream
