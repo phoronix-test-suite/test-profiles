9a41756c90ce56075840823925517a66faa8e3d8
Michael Larabel <no-reply@openbenchmarking.org>
2018-02-15 14:07:12 -0600

    pts/pybench-1.0.1: Be explicit about python2, don't use generic python call as this test will break when python is python3.

---
+++ pts/pybench-1.0.1/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+tar -zxvf pybench-2009-08-14.tar.gz
+
+echo "#!/bin/sh
+cd pybench-2009-08-14/
+python2 pybench.py \$@ > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > pybench
+
+chmod +x pybench
