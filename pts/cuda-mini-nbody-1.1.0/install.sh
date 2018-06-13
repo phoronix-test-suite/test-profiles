c024669a1f34d92d4dec71b6b35b6dc5b43a84fc
Alberto Spina <alberto.spina15@imperial.ac.uk>
2018-06-13 15:31:38 +0100

    pts/cuda-mini-nbody-1.1.0/install.sh: Remove commented out redundant installation code

--- pts/cuda-mini-nbody-1.1.0/install.sh
+++ pts/cuda-mini-nbody-1.1.0/install.sh
@@ -3,9 +3,6 @@
 unzip -o mini-nbody-20151110.zip
 cd mini-nbody-master/cuda
 
-#cc -std=c99 -O3 -fopenmp -DSHMOO -o test-nbody nbody-orig.cu -lm
-#echo $? > ~/install-exit-status
-
 cd ~/
 echo "#!/bin/sh
 
