c9965d2915c017bfca0edfb8d60953603778c895
loader <loader@FreeBSD.org>
2018-10-14 02:53:27 +0000

    Allow building and running pts/fio-1.11.2 on FreeBSD

--- pts/fio-1.11.2/install.sh
+++ pts/fio-1.11.2/install.sh
@@ -3,7 +3,12 @@
 tar -xzf fio-3.1.tar.gz
 cd fio-3.1/
 ./configure
-make -j $NUM_CPU_JOBS
+if [ "$OS_TYPE" = "BSD" ]
+then
+	gmake -j $NUM_CPU_JOBS
+else
+	make -j $NUM_CPU_JOBS
+fi
 echo $? > ~/install-exit-status
 cd ~
 
@@ -25,9 +30,14 @@ buffered=\$3
 startdelay=5
 ramp_time=5
 runtime=20
-time_based
-disk_util=0
-clat_percentiles=0
+time_based\" > test.fio
+
+if [ \"\${OPERATING_SYSTEM}\" != \"freebsd\" ]
+then
+	echo \"disk_util=0\" >> test.fio
+fi
+
+echo \"clat_percentiles=0
 disable_lat=1
 disable_clat=1
 disable_slat=1
@@ -37,7 +47,7 @@ filename=fiofile
 [test]
 name=test
 bs=\$5
-stonewall\" > test.fio
+stonewall\" >> test.fio
 
 ./fio test.fio 2>&1 > \$LOG_FILE" > fio-run
 chmod +x fio-run
