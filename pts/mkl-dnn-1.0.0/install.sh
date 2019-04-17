b131dbce60a641679dec398aec9b6859a7a81457
Michael Larabel <no-reply@openbenchmarking.org>
2019-04-16 19:44:47 -0500

    pts/mkl-dnn-1.0.0: Initial commit of Intel MKL-DNN benchdnn benchmark.

---
+++ pts/mkl-dnn-1.0.0/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+tar -xf mkl-dnn-20190416.tar.xz
+
+cd mkl-dnn-master/
+./scripts/prepare_mkl.sh 
+
+mkdir build 
+cd build 
+cmake $CMAKE_OPTIONS ..
+make -j $NUM_CPU_CORES
+echo $? > ~/install-exit-status
+
+cd ~
+
+echo "#!/bin/sh
+cd mkl-dnn-master/build/tests/benchdnn
+./benchdnn \$@ > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > mkl-dnn
+chmod +x mkl-dnn
