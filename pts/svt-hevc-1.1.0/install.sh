6ebddac7880e0ab4dbff8f2d5cc643d2b43fe6d2
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2019-09-11 16:34:42 +0200

    Update SVT-HEVC profile for v1.4.1 release

--- pts/svt-hevc-1.1.0/install.sh
+++ pts/svt-hevc-1.1.0/install.sh
@@ -2,8 +2,8 @@
 
 7z x Bosphorus_1920x1080_120fps_420_8bit_YUV_RAW.7z
 
-unzip -o SVT-HEVC-20190906.zip
-cd SVT-HEVC-master/
+unzip -o SVT-HEVC-1.4.1.zip
+cd SVT-HEVC
 mkdir build
 cd build
 cmake ..
@@ -13,6 +13,6 @@ echo $? > ~/install-exit-status
 cd ~
 
 echo "#!/bin/sh
-./SVT-HEVC-master/Bin/Release/SvtHevcEncApp -i Bosphorus_1920x1080_120fps_420_8bit_YUV.yuv -w 1920 -h 1080 > \$LOG_FILE 2>&1
+./SVT-HEVC-master/Bin/Release/SvtHevcEncApp -encMode 5 -i Bosphorus_1920x1080_120fps_420_8bit_YUV.yuv -w 1920 -h 1080 > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > svt-hevc
 chmod +x svt-hevc
