38ff5d392d590619813381bfc41cb5e045c41c1a
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2019-11-12 14:08:00 +0100

    Update install.sh by removing parameters

--- pts/aom-av1-2.0.0/install.sh
+++ pts/aom-av1-2.0.0/install.sh
@@ -10,7 +10,7 @@ cd ~
 7z x Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M.7z
 
 echo "#!/bin/sh
-./aom-20190916/build/aomenc -v --rt --threads=\$NUM_CPU_CORES --tile-columns=2 --limit=20 -o test.av1 Bosphorus_1920x1080_120fps_420_8bit_YUV.y4m > 1.log 2>&1
+./aom-20190916/build/aomenc --threads=\$NUM_CPU_CORES -o test.av1 Bosphorus_1920x1080_120fps_420_8bit_YUV.y4m > 1.log 2>&1
 echo \$? > ~/test-exit-status
 sed \$'s/[^[:print:]\t]/\\n/g' 1.log > \$LOG_FILE" > aom-av1
 chmod +x aom-av1
