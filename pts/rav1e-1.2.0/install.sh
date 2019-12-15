016c86cf361ce31e9ccb0109549a83ba349a853d
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2019-12-15 16:01:47 +0100

    Update all other files to rav1e p20191215

--- pts/rav1e-1.2.0/install.sh
+++ pts/rav1e-1.2.0/install.sh
@@ -2,14 +2,14 @@
 
 7z x Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M.7z
 
-tar -xf rav1e-p20191201.tar.gz
-cd rav1e-p20191201
+tar -xf rav1e-p20191215.tar.gz
+cd rav1e-p20191215
 cargo build --bin rav1e --release -j $NUM_CPU_PHYSICAL_CORES
 echo $? > ~/install-exit-status
 
 cd ~
 echo "#!/bin/sh
-cd rav1e-p20191201
+cd rav1e-p20191215
 ./target/release/rav1e ../Bosphorus_1920x1080_120fps_420_8bit_YUV.y4m --threads \$NUM_CPU_CORES -tiles 4 --output /dev/null > log.out 2>&1
 echo \$? > ~/test-exit-status
 
