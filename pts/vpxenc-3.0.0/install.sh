a386a3f8c3ad83806f0661f021f4110ef0786a75
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2019-12-24 11:06:59 +0100

    Add two speed levels, remove unneccessary options
     - Adds two speed levels: Speed 5 and Speed 0
    - Removes the following options: --codec=vp9 -v --threads=\$THREADCOUNT --tile-columns=6 --row-mt=1
     - All removed parameters are either enabled by default or do not increase encoding speed

--- pts/vpxenc-3.0.0/install.sh
+++ pts/vpxenc-3.0.0/install.sh
@@ -5,7 +5,7 @@ tar -xzf libvpx-1.8.2.tar.gz
 mkdir vpx
 cd libvpx-1.8.2
 
-./configure --disable-install-docs --enable-shared --prefix=$HOME/vpx
+./configure --disable-install-docs --disable-vp8 --enable-shared --prefix=$HOME/vpx
 make -j $NUM_CPU_CORES
 echo $? > ~/install-exit-status
 make install
@@ -17,6 +17,6 @@ rm -rf libvpx-1.8.2
 echo "#!/bin/bash
 cd vpx/bin
 THREADCOUNT=\$((\$NUM_CPU_CORES>64?64:\$NUM_CPU_CORES))
-LD_PRELOAD=../lib/libvpx.so  ./vpxenc --codec=vp9 -v --threads=\$THREADCOUNT --tile-columns=6 --row-mt=1 -o /dev/null ~/Bosphorus_1920x1080_120fps_420_8bit_YUV.yuv --width=1920 --height=1080 2> \$LOG_FILE
+LD_PRELOAD=../lib/libvpx.so  ./vpxenc -o /dev/null ~/Bosphorus_1920x1080_120fps_420_8bit_YUV.yuv --width=1920 --height=1080 2> \$LOG_FILE
 echo \$? > ~/test-exit-status" > vpxenc
 chmod +x vpxenc
