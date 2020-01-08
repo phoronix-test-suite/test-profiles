0f6b9076da2253a2f15763786814d413995392a9
pnp <pnp@clear-my.sh.intel.com>
2020-01-08 02:15:35 +0000

    upgrade scikit-learn benchmark to 0.22.1 version

---
+++ pts/scikit-learn-1.0.2/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+tar xvf scikit-learn-0.22.1.tar.gz
+
+echo "#!/bin/sh
+cd scikit-learn-0.22.1/benchmarks/
+python bench_random_projections.py
+echo \$? > ~/test-exit-status" > scikit-learn
+chmod +x scikit-learn
