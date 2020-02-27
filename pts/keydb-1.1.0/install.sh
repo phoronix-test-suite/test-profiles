a8f22f3cc77af1d459ec4c5ab801bd07e7965518
wangyang <wangyang.guo@intel.com>
2020-02-27 15:20:27 +0800

    use memtier to benchmark KeyDB

--- pts/keydb-1.1.0/install.sh
+++ pts/keydb-1.1.0/install.sh
@@ -10,6 +10,14 @@ make MALLOC=libc -j $NUM_CPU_CORES
 echo $? > ~/install-exit-status
 
 cd ~
+tar -xf memtier_benchmark-1.2.17.tar.gz
+cd memtier_benchmark-1.2.17/
+autoreconf -ivf
+./configure
+make -j $NUM_CPU_CORES
+echo $? > ~/install-exit-status
+
+cd ~
 echo "#!/bin/sh
 cd ~/KeyDB-5.0.2/
 
@@ -17,7 +25,9 @@ cd ~/KeyDB-5.0.2/
 KEYDB_SERVER_PID=\$!
 sleep 8
 
-./src/keydb-benchmark --threads \$NUM_CPU_CORES \$@ > \$LOG_FILE
+cd ~/memtier_benchmark-1.2.17/
+./memtier_benchmark --hide-histogram -t \$NUM_CPU_CORES \$@ > \$LOG_FILE
+
 kill \$KEYDB_SERVER_PID
 sleep 2" > keydb
 chmod +x keydb
