c9a4c8a5d6a07b21751cfd208aad7f4674695fe4
oneoa <74601428+oneoa@users.noreply.github.com>
2020-11-27 20:28:44 +1000

    Update install.sh

--- pts/glmark2-1.1.0/install.sh
+++ pts/glmark2-1.1.0/install.sh
@@ -3,6 +3,13 @@
 tar -zxvf glmark2-20170617.tar.gz
 cd glmark2-20170617
 
+#Fix for python 3.7 and up
+if [[ $(python --version | grep '[0-9]\.[0-9]' -o | tr -d '.' ) -ge 37 ]]
+then
+        sed -i "/StopIteration/d" ./waflib/Node.py
+        sed -i "/StopIteration/d" ./waflib/Tools/qt5.py
+fi
+
 ./waf configure --with-flavors=x11-gl --prefix=$HOME
 ./waf build
 ./waf install
