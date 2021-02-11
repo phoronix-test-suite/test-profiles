78b5a32d1e8fc382d37d50e24b1c4dad1cda4ff7
Michael Larabel <no-reply@openbenchmarking.org>
2021-02-11 15:14:17 -0600

    pts/jpegxl-1.0.1: Add Brotli as a system dependency.

---
+++ pts/jpegxl-1.0.1/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+tar -xf jpeg-xl-v0.3.1.tar.gz
+unzip -o sample-photo-6000x4000-1.zip
+
+cd jpeg-xl-v0.3.1/
+./deps.sh
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
+make -j $NUM_CPU_CORES
+echo $? > ~/install-exit-status
+
+cd ~
+
+echo "#!/bin/sh
+./jpeg-xl-v0.3.1/build/tools/cjxl sample-photo-6000x4000.JPG out.jxl \$@ > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > jpegxl
+chmod +x jpegxl
