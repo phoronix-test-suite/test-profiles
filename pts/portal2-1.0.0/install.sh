46a8dfd275c506859c032831150c6320be518261
Michael Larabel <no-reply@openbenchmarking.org>
2021-02-19 08:33:03 -0600

    pts/portal2-1.0.0: Initial commit of Portal 2 benchmark with Vulkan and OpenGL support.

---
+++ pts/portal2-1.0.0/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+HOME=$DEBUG_REAL_HOME steam steam://install/620
+echo $? > ~/install-exit-status
+
+unzip -o portal2-demo-pts2.zip
+mv pts2.dem $DEBUG_REAL_HOME/.steam/steam/steamapps/common/Portal\ 2/portal2
+
+echo "#!/bin/bash
+. steam-env-vars.sh
+cd \$DEBUG_REAL_HOME/.steam/steam/steamapps/common/Portal\ 2
+
+./portal2_linux -game portal2 +con_logfile \$LOG_FILE +cl_showfps 1 +timedemoquit pts2 -novid -fullscreen \$@" > portal2
+chmod +x portal2
