7d57b257ea3d703c2c34e37d98b1d1f31ee6bb22
tad4 <terrace.modems0f@icloud.com>
2023-01-05 16:20:56 -0500

    Update the invocation for the app under test---i.e., CS:GO---to use the Steam CLI API---and add logic for the test in progress to wait for the app under test to exit before ingesting $LOG_FILE.

--- pts/csgo-1.7.2/install_windows.sh
+++ pts/csgo-1.7.2/install_windows.sh
@@ -18,7 +18,9 @@ echo "#!/bin/sh
 cd \"${STEAMPATH}/steamapps/common/Counter-Strike Global Offensive\"
 rm -rf csgo/SourceBench*
 rm -f UNKNOWN
-./csgo.exe -game csgo \$@ +con_logfile log.log
+../../../steam.exe -applaunch 730 \$@ +con_logfile log.log
+sleep 6 # Short wait for Steam to start the CS:GO process.
+powershell.exe -NonInteractive -Command \"Wait-Process csgo\"
 cat csgo/log.log* > \$LOG_FILE
 cat csgo/SourceBench* >> \$LOG_FILE
 cat csgo/UNKNOWN >> \$LOG_FILE" > csgo
