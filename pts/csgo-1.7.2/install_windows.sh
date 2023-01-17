b7ebb65ba8408c9ce52574f953ef6ea9378faac4
tad4 <121957038+tad4@users.noreply.github.com>
2023-01-17 14:09:12 -0500

    Add support for spaces in the STEAMPATH

--- pts/csgo-1.7.2/install_windows.sh
+++ pts/csgo-1.7.2/install_windows.sh
@@ -2,14 +2,14 @@
 
 if /cygdrive/c/windows/system32/reg.exe query "HKEY_CURRENT_USER\Software\Valve\Steam" /v SteamPath
 then
-    STEAMPATH=`/cygdrive/c/windows/system32/reg.exe query "HKEY_CURRENT_USER\Software\Valve\Steam" /v SteamPath | awk '$1 ~ "SteamPath" && $2 ~ "REG_SZ" { print(substr($3, 1, length($3)-1)) }'`
+    STEAMPATH=`/cygdrive/c/windows/system32/reg.exe query "HKEY_CURRENT_USER\Software\Valve\Steam" /v SteamPath | awk '$1 ~ "SteamPath" && $2 ~ "REG_SZ" { ORS=""; for (i = 3; i < NF; i++) print($i " "); print(substr($NF, 1, length($NF)-1)) }'`
 else
     echo "ERROR: Steam is not found on the system! This test profile needs to find Steam configuration data in the Windows Registry."
 	echo 2 > ~/install-exit-status
     exit
 fi
 
-/cygdrive/${STEAMPATH:0:1}/${STEAMPATH:3}/steam.exe "steam://install/730"
+"/cygdrive/${STEAMPATH:0:1}/${STEAMPATH:3}/steam.exe" "steam://install/730"
 
 unzip -o csgo-demo-10.zip
 mv pts10.dem "${STEAMPATH}/steamapps/common/Counter-Strike Global Offensive/csgo"
@@ -26,3 +26,19 @@ cat csgo/SourceBench* >> \$LOG_FILE
 cat csgo/UNKNOWN >> \$LOG_FILE" > csgo
 chmod +x csgo
 echo 0 > ~/install-exit-status
+#!/bin/sh
+
+steam steam://install/730
+
+unzip -o csgo-demo-10.zip
+mv pts10.dem "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo"
+
+echo "#!/bin/sh
+cd \"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\"
+rm -rf csgo/SourceBench*
+rm -f UNKNOWN
+./csgo.exe -game csgo \$@ +con_logfile log.log
+cat csgo/log.log* > \$LOG_FILE
+cat csgo/SourceBench* >> \$LOG_FILE
+cat csgo/UNKNOWN >> \$LOG_FILE" > csgo
+chmod +x csgo
