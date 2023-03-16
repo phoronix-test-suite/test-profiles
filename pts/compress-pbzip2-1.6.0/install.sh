48b13b714f2b476de401dcbbfa0f6f8f7d014a07
Lucian Popescu <lucian.popescu187@gmail.com>
2023-03-16 20:04:28 +0200

    Delete hardcoded compiler

--- pts/compress-pbzip2-1.6.0/install.sh
+++ pts/compress-pbzip2-1.6.0/install.sh
@@ -2,12 +2,16 @@
 
 tar -zxvf bzip2-1.0.8.tar.gz
 tar -zxvf pbzip2-1.1.13.tar.gz
+
+grep -rl "CC=gcc" . | xargs sed -i '/CC=gcc/d'
+
 cd bzip2-1.0.8/
 make
 cp -f libbz2.a ../pbzip2-1.1.13
 cp -f bzlib.h ../pbzip2-1.1.13
 cd ..
 cd pbzip2-1.1.13/
+grep -rl PRIuMAX . | xargs sed -i 's/PRIuMAX/ PRIuMAX /g'
 make pbzip2-static
 echo $? > ~/install-exit-status
 
