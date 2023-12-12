46168273657f1b0aa11c30435825d3eaa91d6c60
Andre Puschmann <andre@srs.io>
2023-12-12 12:35:37 +0100

    update srsran pts to new 23.10.1 release

--- pts/srsran-2.2.0/install.sh
+++ pts/srsran-2.2.0/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-tar -xf srsRAN_Project-release_23_5.tar.gz
-cd srsRAN_Project-release_23_5/
+tar -xf srsRAN_Project-release_23_10_1.tar.gz
+cd srsRAN_Project-release_23_10_1/
 mkdir build
 cd build
 cmake -DENABLE_GUI=OFF -DCMAKE_BUILD_TYPE=Release ..
@@ -8,7 +8,7 @@ make -j $NUM_CPU_CORES
 echo $? > ~/install-exit-status
 cd ~
 echo "#!/bin/sh
-cd srsRAN_Project-release_23_5/build/
+cd srsRAN_Project-release_23_10_1/build/
 ./\$@ 2>&1 > \$LOG_FILE
 echo \$? > ~/test-exit-status" > srsran
 chmod +x srsran
