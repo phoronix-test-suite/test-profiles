3445899118fbf2bd38846fc7b7ce0a269b9c942c
Michael Larabel <no-reply@openbenchmarking.org>
2024-06-08 00:25:07 +0000

    pts/gcrypt-1.2.0: Update against latest gcrypt 1.10.3 upstream release. Closes: https://github.com/phoronix-test-suite/phoronix-test-suite/issues/786

---
+++ pts/gcrypt-1.2.0/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+mkdir $HOME/gpgerror
+tar -jxf libgpg-error-1.41.tar.bz2
+cd libgpg-error-1.41/
+./configure --prefix=$HOME/gpgerror
+make -j $NUM_CPU_CORES
+echo $? > ~/install-exit-status
+make install
+cd ~
+rm -rf libgpg-error-1.41/
+tar -jxf libgcrypt-1.10.3.tar.bz2
+cd libgcrypt-1.10.3
+./configure --with-gpg-error-prefix=$HOME/gpgerror
+make -j $NUM_CPU_CORES
+echo $? > ~/install-exit-status
+cd ~
+echo "#!/bin/sh
+./libgcrypt-1.10.3/tests/benchmark \$@ > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > gcrypt
+chmod +x gcrypt
+
+
